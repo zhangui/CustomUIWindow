62eddf228fddd3816b3ebfb25eb4dbb5d0b6dd63
Yang Zhang <yang10.zhang@gmail.com>
2019-02-10 14:05:46 -0500

    Initial commit

---
+++ Example/CustomUIWindow/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CustomUIWindow
+//
+//  Created by zhangui on 02/10/2019.
+//  Copyright (c) 2019 zhangui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
